7cf9a96de889e823cdf85c09bfbb87fb1a412b5e
zono <t.ishizono@futurek.com>
2025-08-13 21:26:28 +0900

    feat: :sparkles: Added tray configure.

--- main.js
+++ main.js
@@ -13,6 +13,10 @@ const globalShortcut = electron.globalShortcut;
 // メインウィンドウはGCされないようにグローバル宣言
 let captureWindow = null;
 
+// システムトレイアイコンの追加
+const path = require('path');
+const { Tray, nativeImage } = require('electron');
+
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = '1';
 
 // 全てのウィンドウが閉じたら終了
@@ -22,6 +26,13 @@ app.on('window-all-closed', function () {
     // }
 });
 
+/**
+ * Opens a capture window if an image is available in the clipboard. The size of the window
+ * will match the dimensions of the image. The window is created with specific configurations
+ * such as being frameless, non-resizable, and always staying on top.
+ *
+ * @return {void} Returns nothing. The function creates and displays a new window if an image is available.
+ */
 function openCaptureWindow(){
     // クリップボードから画像を読み取り
     const image = clipboard.readImage();
@@ -58,6 +69,26 @@ function openCaptureWindow(){
     }
 }
 
+/**
+ * Retrieves the file path of the tray icon based on the application environment.
+ * If the application is packaged, it uses the `process.resourcesPath` to locate the resource directory.
+ * If the application is in development, it uses `__dirname` to locate the resource.
+ *
+ * @return {string} The file path to the tray icon in the application's assets directory.
+ */
+function getTrayIconPath() {
+    // 開発時: __dirname はプロジェクト内の main.js がある場所
+    // パッケージ後: __dirname は app.asar 内の main.js がある場所
+    // 多くの場合 __dirname ベースでも動きますが、
+    // アイコンを extraResources に出す場合は process.resourcesPath を使います。
+    if (app.isPackaged) {
+        // extraResources に出す構成の場合はこちら
+        return path.join(process.resourcesPath, 'assets', 'icons', 'png', '32x32.png');
+    } else {
+        return path.join(__dirname, 'assets', 'icons', 'png', '32x32.png');
+    }
+}
+
 // Electronの初期化完了後に実行
 app.on('ready', function () {
 
@@ -68,6 +99,11 @@ app.on('ready', function () {
         console.log('registration failed')
     }
 
+    // システムトレイアイコン（オプション）
+    const iconPath = getTrayIconPath();
+    const icon = nativeImage.createFromPath(iconPath);
+    const tray = new Tray(icon);
+    tray.setToolTip('kiritoru2');
 });
 
 // clipboardの読み書き機能をIPCで公開
